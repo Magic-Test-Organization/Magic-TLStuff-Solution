a336ac2ab9926e4aea0f726f3e8df3d66b487dc0
Brian Doyle <briandoyle81@gmail.com>
2020-01-07 17:34:35 -0600

    Update added2.py

--- src/added2.py
+++ src/added2.py
@@ -2,3 +2,29 @@
 
 # maybe it can't be broken?
 
+#where is it?
+#now?
+
+# funny elsif?
+
+# $%$Start
+print("this is fine")
+# $%$End
+
+# $%$Start
+# this should fail
+
+
+# $%$BadToken
+# $%$AlsoBad
+
+
+# $%$End
+
+
+# $%$Start
+
+# what will be updated?
+
+# broken file is broken
+
