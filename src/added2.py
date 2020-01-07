c11e94325f3cc018d5fda8ee1074b2f69ebf3362
Brian Doyle <briandoyle81@gmail.com>
2020-01-07 17:21:26 -0600

    Update added2.py

--- src/added2.py
+++ src/added2.py
@@ -1,28 +1,4 @@
 #second addded file it's the elseif ,,,,now? it works
 
-#where is it?
-#now?
-
-# funny elsif?
-
-# $%$Start
-print("this is fine")
-# $%$End
-
-# $%$Start
-# this should fail
-
-
-# $%$BadToken
-# $%$AlsoBad
-
-
-# $%$End
-
-
-# $%$Start
-
-# what will be updated?
-
-# broken file is broken
+# maybe it can't be broken?
 
